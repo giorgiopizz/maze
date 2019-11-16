95163c9749a9f79e16a77b79745a77bdeb3fad8f
Giorgio Pizzati <giorgio.pizzati@gmail.com>
2019-11-16 12:33:36 +0100

    long run done

--- maze6.py
+++ maze6.py
@@ -5,21 +5,20 @@ from maze5 import *
 import os
 
 if __name__=="__main__":
-    n=1000
+    n=100000
     sum=0
     percorso="primo"
     if not os.path.exists(percorso):
         os.mkdir(percorso)
     for i in range(5,7,2):
         vec=[]
-        for _ in range(n):
+        for l in range(n):
             #creating n maze and solving them
             create_maze(i)
             j=solve_maze(percorso+"/",str(i))
             vec.append(j)
-            #little break
-            if(n//25):
-                sleep(30)
+            if(l%1000==0):
+                sleep(5)
         #saving the n moves
         file=open("dati-"+str(i)+".txt","a")
         for j in vec:
